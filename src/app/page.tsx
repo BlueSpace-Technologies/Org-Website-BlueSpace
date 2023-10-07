e75f2ed27ccd26db9a20b6eb26dbc29875e26d51
Sumeet Vishwakarma <85386116+LegendSumeet@users.noreply.github.com>
2023-10-07 19:46:44 +0530

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Avatar from "@/components/ui/avatar";
 import Navbar from "@/components/NavBar/Navbar";
 import HeroSection from "@/components/hero-section/HeroSection";
 
+
+
 export default function Home() {
   return (
     <main className="overflow-hidden bg-black">
--- src/components/hero-section/HeroSection.tsx
+++ src/components/hero-section/HeroSection.tsx
@@ -4,6 +4,7 @@ import React from "react";
 
 const HeroSection = () => {
   return (
+    
     <div className="flex flex-auto flex-col items-start p-10 mt-10">
       <h1 className="font-bold text-white text-[52px] sm:text-[50px] text-start md:text-start lg:text-start">
         BlueSpace is a Tech branding
@@ -23,16 +24,30 @@ const HeroSection = () => {
           </video>
         </div>
       </div>
-      <div className="mt-[20] p-56 justify-start ">
-        <div className=" justify-start items-center">
-          <h1 className="font font-semibold text-[30px]">
+      <div className="flex flex-col lg:flex-row p-4 sm:p-10 md:p-20 lg:p-40">
+        <div className="flex-grow lg:flex-grow-0 ml-4 lg:ml-0 mb-4 lg:mb-0">
+          <h1 className="font-bold text-white text-2xl sm:text-3xl text-center sm:text-left md:text-left lg:text-left">
             Based in Mumbai, we strengthen <br />
             brands through exceptional digital <br /> experiences for companies
             worldwide.
           </h1>
         </div>
-    
+        
+        
+
+        <div className="flex-grow  lg:flex-grow-0 ml-65 lg:ml-65 lg: mb-4 lg:mb-0 lg:mt-0 sm:mt-0">
+          <h1 className="font-bold text-white text-2xl sm:text-3xl text-center sm:text-left md:text-left lg:text-left">
+            We are a team of designers, <br />
+            developers, and strategists <br />
+            who are passionate about <br />
+            creating exceptional digital <br />
+            experiences that unite people <br />
+            and brands.
+          </h1>
+        </div>
       </div>
+
+      <div className="flex flex-col lg:flex-row justify-between p-4 sm:p-10 md:p-20 lg:p-40"></div>
     </div>
   );
 };
