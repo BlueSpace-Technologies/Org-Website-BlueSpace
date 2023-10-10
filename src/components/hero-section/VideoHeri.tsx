f64af47c252422de0ab07d82e03c8de813fdfdd6
Sumeet Vishwakarma <85386116+LegendSumeet@users.noreply.github.com>
2023-10-10 18:48:49 +0530

    adde

--- src/components/hero-section/VideoHeri.tsx
+++ src/components/hero-section/VideoHeri.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const VideoHeri = () => {
   return (
     <section>
-      <div className="h-[100%] w-[100%] aspect-[16/9] mt-18 relative ">
+      <div className="h-[100%] w-[100%] aspect-[16/9] mt-18 relative sm:h-full ">
         <div className="absolute inset-0 flex items-center justify-center">
           <video
             poster="https://cdn.sanity.io/images/r115idoc/production/3b4132739409b27731583066a5ea88091a7b4935-1700x954.png?q=90&auto=format"
