d00ec962796b4c0991f66b28f405058dd106bf6e
Sumeet Vishwakarma <85386116+LegendSumeet@users.noreply.github.com>
2023-10-23 21:38:40 +0530

    aaded

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <div className="bg-black">
       <Hero />
-    
         <BRAND />
         <INDUSTRY />
  
--- src/components/hero-section/hero-service.tsx
+++ src/components/hero-section/hero-service.tsx
@@ -1,26 +1,20 @@
+import React from "react";
+
 const Hero = () => {
-    return (
-        <main>
-            <div className=" w-full bg-black lg:min-w-full   md:p-[50px] ">
-        <div className="flex  items-start  lg:pl-[100px] w-full sm:mx-auto lg:mt-8">
-        <h1 className="font-unisans font-bold text-white text-3xl ml-4 lg:text-6xl mt-0 text-start sm:text-5xl">
-            A full-service digital 
-            <br />
-            innovation partner
+  return (
+    <main>
+      <section className="bg-black py-10 px-9 lg:p-40">
+        <h1 className="mb-5 text-white font-unisans font-bold text-5xl lg:text-9xl sm:text-8xl">
+        A full-service digital innovation partner
+        </h1>
+        <div className=" min-w-max py-7">
+        <h1 className="max-w-xl text-white  font-unisans font-none text-lg lg:text-2xl sm:text-3xl">
+        Our rich design and technology expertise delivers top brands and digital experiences.
         </h1>
         </div>
-        
-        <div className="lg:p-[30px] lg:pl-[100px]">
-            <span className="font-unisans text-white text-lg ml-4 lg:text-3xl mt-0 text-start sm:text-5xl">
-            Our rich design and technology <span className="ml-4  lg:ml-0">expertise </span>
-            <br  /> <span className="ml-4"> delivers top brands  and <span className="hidden">digital</span> </span>  <br className="sm:block lg:hidden" />
-            <span className="ml-4 lg:ml-0 sm:inline"> experiences.
-            </span>
-            </span>
-        </div>
-        </div>
-        
-        </main>
-    )
-}
-export default Hero
+      </section>
+    </main>
+  );
+};
+
+export default Hero;
